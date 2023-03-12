111a79aae8badc203d4a36a1206f5de16a6cfe78
JSY <65614582+jjsyeon@users.noreply.github.com>
2023-03-12 23:45:24 +0900

    Update 1157.py

--- baekjoon/1157.py
+++ baekjoon/1157.py
@@ -1,15 +1,31 @@
+# s = list(input().upper())
+# alphabets = list(set(s))
+# times = -1
+# max_index = -1
+# count = 0
+# for times in range(len(alphabets)):
+#     if s.count(alphabets[times]) > count:
+#         count = s.count(alphabets[times])
+#         max_index = times
+#     elif s.count(alphabets[times]) == count:
+#         print('?')
+#         times = 0
+#         break
+
+# if times == len(alphabets) - 1: print(alphabets[max_index])
+
+# 위에 틀림 : 틀린 이유 리뷰
+
 s = list(input().upper())
 alphabets = list(set(s))
 times = -1
 max_index = -1
-count = 0
+cnt_list = []
 for times in range(len(alphabets)):
-    if s.count(alphabets[times]) > count:
-        count = s.count(alphabets[times])
-        max_index = times
-    elif s.count(alphabets[times]) == count:
-        print('?')
-        times = 0
-        break
+    cnt = s.count(alphabets[times])
+    cnt_list.append(cnt)
+    max_index = times
 
-if times == len(alphabets) - 1: print(alphabets[max_index])
+if cnt_list.count(max(cnt_list)) == 1:
+    print(alphabets[cnt_list.index(max(cnt_list))])
+else: print('?')
