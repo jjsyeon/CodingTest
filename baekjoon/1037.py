6d17bbaef059d588af847bf351f39fa6960e6b1b
JSY <65614582+jjsyeon@users.noreply.github.com>
2023-03-13 00:01:37 +0900

    Update 1037.py

--- baekjoon/1037.py
+++ baekjoon/1037.py
@@ -6,3 +6,10 @@ if length % 2 == 0:
     print(numbers[length//2 - 1] * numbers[length//2])
 else:
     print(numbers[length//2] * numbers[length//2])
+
+# or
+
+N = int(input())
+numbers = sorted(list(map(int,input().split())))
+
+print(numbers[0] * numbers[-1])
