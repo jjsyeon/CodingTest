686cde32230dbed127fbd4f1cd6a83061fceb859
JSY <65614582+jjsyeon@users.noreply.github.com>
2023-03-12 17:32:34 +0900

    Create 1157.py

---
+++ baekjoon/1157.py
@@ -0,0 +1,15 @@
+s = list(input().lower())
+alphabets = list(set(s))
+times = -1
+max_index = -1
+count = 0
+for times in range(len(alphabets)):
+    if s.count(alphabets[times]) > count:
+        count = s.count(alphabets[times])
+        max_index = times
+    elif s.count(alphabets[times]) == count:
+        print('?')
+        times = 0
+        break
+
+if times == len(alphabets) - 1: print(alphabets[max_index].upper())
