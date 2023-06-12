d499d49509288ee48ee27ad78104e778096ef0ee
JSY <65614582+jjsyeon@users.noreply.github.com>
2023-06-12 10:53:58 +0900

    update : 1003.py
    백준 1003 파이썬 재풀이

--- baekjoon/1003.py
+++ baekjoon/1003.py
@@ -1,16 +1,14 @@
 T = int(input())
+zeros = [1, 0, 1]
+ones = [0, 1, 1]
 
 def fibonacci(n):
-    if n < 2 : return n
-    val_list = [1,1]
-    for i in range(n-2):
-        val_list.append(val_list[i]+ val_list[i+1])
-    return val_list[-1]
+    if n >= len(zeros):
+        for i in range(len(zeros), n+1):
+            zeros.append(zeros[i-2] + zeros[i-1])
+            ones.append(ones[i-2] + ones[i-1])
+    print(zeros[n], ones[n])
 
-for test_case in range(T):
-    value = int(input())
-
-    if value == 0: print(1,0)
-    elif value == 1: print(0,1)
-    elif value == 2: print(1,1)
-    else : print(fibonacci(value-1), fibonacci(value))
+for t in range(T):
+    num = int(input())
+    fibonacci(num)
