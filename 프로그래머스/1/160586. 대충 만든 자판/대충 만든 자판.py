1b322cb11b9a3fac8075dbbc8c543d55ecfd2d36
JSY <65614582+jjsyeon@users.noreply.github.com>
2024-08-04 15:36:41 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.92 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,17 @@
+def solution(keymap, targets):
+    answer = []
+    key_dict = dict()
+    for key in keymap:
+        for idx, alphabet in enumerate(key):
+            if alphabet in key_dict.keys():
+                key_dict[alphabet] = min(key_dict[alphabet], idx +1)
+            else: key_dict[alphabet] = idx+1
+    for target in targets:
+        temp_answer = 0
+        for t in target:
+            if t in key_dict.keys(): temp_answer += key_dict[t]
+            else : 
+                temp_answer = -1
+                break
+        answer.append(temp_answer)
+    return answer
