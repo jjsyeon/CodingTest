363725f1df25e90dd116723e0661e55a31f69dab
JSY <65614582+jjsyeon@users.noreply.github.com>
2024-08-05 10:53:26 +0900

    [level 2] Title: 과제 진행하기, Time: 2.97 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -3,21 +3,22 @@ def time2int(time) -> int:
     return h * 60 + m
     
 def solution(plans):
+    # 값 초기화
     answer = []
     remains = []
+    current = 0
+    
+    # 시간 문자열 정수로 변환
     plans = [[p[0], time2int(p[1]), p[2]] for p in plans]
     plans.sort(key = lambda x: x[1])
-    # print(plans,"\n----------------------")
-    current = 0
+    
+    # 계획한 과제 or 남은 과제가 있는 경우
     while plans or remains:
         name, start, playtime = plans.pop(0)
-        # print(current, name, start, playtime)
-        # print("plnas : ",plans, "\nremains : ", remains)
         playtime = int(playtime)
         space = start - current
-        # print("space : " , space)
+        # 만약 다음 과제 전에 시간이 남고 끝내지 못한 과제가 남아있는 경우
         while space > 0 and remains:
-            # print(11)
             rn, rp = remains.pop(-1)
             if space >= rp:
                 space -= rp
@@ -27,20 +28,17 @@ def solution(plans):
                 remains.append([rn,rp])
                 break
         
+        # 계획된 과제 수행하기
         current = start
-        try:
+        try: # 이후 계획된 과제 있음
             ns = plans[0][1]
-            # print("supposed end : ", current + playtime)
             if current + playtime <= ns:
-                print(1, current + playtime, ns)
                 answer.append(name)
                 current += playtime
             else:
-                # print(2, current + playtime, ns)
                 remains.append([name, current + playtime - ns])
-                # print(2, remains)
                 current = ns
-        except:
+        except: # 이번이 마지막으로 계획된 과제
             answer.append(name)
             if remains:
                 remains.reverse()
