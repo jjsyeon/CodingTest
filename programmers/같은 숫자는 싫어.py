8b5b2ed8025afc63df8b7c404a2bd9cf0bff686b
JSY <65614582+jjsyeon@users.noreply.github.com>
2024-08-07 02:57:09 +0900

    Delete programmers directory

--- programmers/같은 숫자는 싫어.py
+++
@@ -1,8 +0,0 @@
-def solution(arr):
-    answer = []
-    before_val = -1
-    for item in arr:
-        if item != before_val:
-            answer.append(item)
-            before_val = item
-    return answer
