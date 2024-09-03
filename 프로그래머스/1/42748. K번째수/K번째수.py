51345c790ddd82633223abbf05246456a384de2e
JSY <65614582+jjsyeon@users.noreply.github.com>
2024-09-03 21:30:19 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,9 @@
 def solution(array, commands):
     answer = []
-    for command in commands:
-        sorted_array = sorted(array[command[0]-1:command[1]])
-        answer.append(sorted_array[command[2]-1])
+    for cmd in commands:
+        i,j,k = cmd
+        temp = array[i-1:j]
+        temp.sort()
+        answer.append(temp[k-1])
+        
     return answer
