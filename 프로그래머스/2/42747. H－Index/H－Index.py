db02fb5ec6165108005ca10cd06ff07e210a97f2
JSY <65614582+jjsyeon@users.noreply.github.com>
2024-09-09 12:13:00 +0900

    [level 2] Title: H-Index, Time: 589.57 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -2,7 +2,6 @@ def solution(citations):
     citations.sort(reverse=True)
     for h in range(citations[0], -1, -1):
         for idx in range(len(citations)):
-            if citations[idx] < h:
-                break
+            if citations[idx] < h:break
             if idx == len(citations)-1: idx = len(citations)
         if idx >= h: return h
