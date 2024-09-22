25216aa46afc17afca12b64384ddce418728b981
JSY <65614582+jjsyeon@users.noreply.github.com>
2024-09-23 01:22:21 +0900

    [level 3] Title: 불량 사용자, Time: 8719.56 ms, Memory: 1.92 GB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,25 @@
+from itertools import product
+
+def is_candidate(u_id, b_id):
+    if len(u_id) != len(b_id): return 0
+    for idx in range(len(b_id)):
+        if b_id[idx] != "*" and b_id[idx] != u_id[idx]: return 0
+    return 1
+
+def solution(user_id, banned_id):
+    candidate_lst = []
+    for b_id in banned_id:
+        candidate = []
+        for u_id in user_id:
+            if is_candidate(u_id, b_id): candidate.append(u_id)
+        candidate_lst.append(candidate) 
+    # print(candidate_lst,'\n')
+    pool = []
+    # print(list(product(*candidate_lst)),'\n')
+    for case in list(product(*candidate_lst)):
+        case = set(case)
+        if len(case) == len(banned_id): 
+            pool.append(" ".join(sorted(case)))
+    pool = set(pool)
+    # print(pool)
+    return len(pool)
