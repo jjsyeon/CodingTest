b1b6f9cc7266542b93c1bbcb57e1f4b68746ca2c
JSY <65614582+jjsyeon@users.noreply.github.com>
2024-12-09 01:51:11 +0900

    [Silver I] Title: 킥다운, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1195. 킥다운/킥다운.py
@@ -0,0 +1,33 @@
+short_part = input()
+long_part = input()
+
+if len(short_part) > len(long_part):
+    short_part, long_part = long_part, short_part
+
+do_more = 1
+length = len(long_part)
+for i in range(length - len(short_part) + 1):
+    for j in range(length - len(long_part) + 1):
+        tmp_short = "0"*i + short_part + "0"*(length-len(short_part)-i)
+        tmp_long = "0"*j + long_part + "0"*(length-len(long_part)-j)
+            
+        is_impossible = sum([(int(tmp_short[k])+int(tmp_long[k]))//4 for k in range(length)])
+        if not is_impossible: do_more = 0
+
+while length <= len(short_part) + len(long_part) and do_more:
+    length += 1
+    overlapped = len(short_part) + len(long_part) - length 
+    # case1
+    tmp_short = short_part[-overlapped:]
+    tmp_long = long_part[:overlapped]
+    is_impossible = sum([(int(tmp_short[k])+int(tmp_long[k]))//4 for k in range(overlapped)])
+    if not is_impossible: 
+        do_more = 0
+        continue
+    # case 2
+    tmp_short = short_part[:overlapped]
+    tmp_long = long_part[-overlapped:]
+    is_impossible = sum([(int(tmp_short[k])+int(tmp_long[k]))//4 for k in range(overlapped)])
+    if not is_impossible: do_more = 0
+
+print(length)
