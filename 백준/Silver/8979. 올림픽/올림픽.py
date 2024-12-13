7dd1ebf7d92319112594240d9f26f683eb24a460
JSY <65614582+jjsyeon@users.noreply.github.com>
2024-12-13 17:32:39 +0900

    [Silver V] Title: 올림픽, Time: 32 ms, Memory: 32412 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+medals = dict()
+for _ in range(n):
+    inputs = list(map(int, input().split()))
+    medals[inputs[0]] = inputs[1:]
+
+answer = 1
+for country in medals.keys():
+    if country == k: continue
+    for i in range(3):
+        if medals[k][i] > medals[country][i] :
+            answer += 1
+            break
+        elif medals[k][i] < medals[country][i] :
+            break
+
+print(answer) 
