7c972fd142a60a0dcc9525119a35dd9bca90730e
JSY <65614582+jjsyeon@users.noreply.github.com>
2024-12-15 17:29:20 +0900

    [Silver III] Title: 다중 일차 함수, Time: 384 ms, Memory: 48308 KB -BaekjoonHub

--- 백준/Silver/26258. 다중 일차 함수/다중 일차 함수.py
+++ 백준/Silver/26258. 다중 일차 함수/다중 일차 함수.py
@@ -7,13 +7,12 @@ graph = dict()
 for _ in range(n):
     x, y = map(int, input().split())
     graph[x] = y
-
 sorted_keys = sorted(graph.keys())
 
 q = int(input())
 for _ in range(q):
-    x = float(input())
-    min_x, max_x = sorted_keys[bisect_left(sorted_keys, x)-1], sorted_keys[bisect_left(sorted_keys, x)]
+    idx = bisect_left(sorted_keys, float(input()))
+    min_x, max_x = sorted_keys[idx-1], sorted_keys[idx]
     if graph[min_x] > graph[max_x]: print(-1)
     elif graph[min_x] < graph[max_x]: print(1)
     else: print(0)
