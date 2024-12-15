5dfa7eab36607bd36e0f047a7059a3ed9802adb7
JSY <65614582+jjsyeon@users.noreply.github.com>
2024-12-15 17:24:49 +0900

    [Silver III] Title: 다중 일차 함수, Time: 448 ms, Memory: 48308 KB -BaekjoonHub

--- 백준/Silver/26258. 다중 일차 함수/다중 일차 함수.py
+++ 백준/Silver/26258. 다중 일차 함수/다중 일차 함수.py
@@ -1,21 +1,19 @@
 import sys
+from bisect import bisect_left
 input = sys.stdin.readline
 
-def binary_search(target):
-    min_x, max_x = 0, len(graph)-1
-    while max_x - min_x > 1:
-        mid_x = (min_x + max_x) // 2
-        if graph[mid_x][0] < target : min_x = mid_x
-        elif graph[mid_x][0] > target : max_x = mid_x
-    return min_x, max_x
-
 n = int(input())
-graph = sorted([list(map(int, input().split())) for _ in range(n)], key=lambda x : x[0])
+graph = dict()
+for _ in range(n):
+    x, y = map(int, input().split())
+    graph[x] = y
+
+sorted_keys = sorted(graph.keys())
 
 q = int(input())
 for _ in range(q):
     x = float(input())
-    min_x, max_x = binary_search(x)
-    if graph[min_x][1] > graph[max_x][1]: print(-1)
-    elif graph[min_x][1] < graph[max_x][1]: print(1)
+    min_x, max_x = sorted_keys[bisect_left(sorted_keys, x)-1], sorted_keys[bisect_left(sorted_keys, x)]
+    if graph[min_x] > graph[max_x]: print(-1)
+    elif graph[min_x] < graph[max_x]: print(1)
     else: print(0)
