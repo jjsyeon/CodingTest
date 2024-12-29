b6d602428b29e0351bbb38c6cd45955307ed4b92
JSY <65614582+jjsyeon@users.noreply.github.com>
2024-12-29 23:07:00 +0900

    [Gold V] Title: 단어 만들기, Time: 1208 ms, Memory: 85324 KB -BaekjoonHub

--- 백준/Gold/1148. 단어 만들기/단어 만들기.py
+++ 백준/Gold/1148. 단어 만들기/단어 만들기.py
@@ -6,9 +6,13 @@ word_dict = []
 while (word:=input().rstrip()) != '-':
     word_dict.append(Counter(word))
 
+check_lst = []
 while (board:=input().rstrip()) != '#':
-    board = Counter(board)
+    check_lst.append(Counter(board))
+
+for board in check_lst:
     frequency = {k:0 for k in board.keys()}
+    
     for word in word_dict:
         for k, v in word.items():
             if k not in board.keys() or v > board[k]: break
@@ -16,5 +20,6 @@ while (board:=input().rstrip()) != '#':
             for w in word.keys(): frequency[w] += 1
 
     min_, max_ = min(frequency.values()), max(frequency.values())
+    
     print(''.join(sorted([k for k in frequency.keys() if frequency[k] == min_])), min_, end=' ')
     print(''.join(sorted([k for k in frequency.keys() if frequency[k] == max_])), max_)
