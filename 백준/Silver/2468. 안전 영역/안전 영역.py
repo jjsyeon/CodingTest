ba870020df4e8bd53f83c2128491d03ca25ceac7
JSY <65614582+jjsyeon@users.noreply.github.com>
2025-02-11 14:07:58 +0900

    [Silver I] Title: 안전 영역, Time: 820 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,17 +1,23 @@
 import sys
+from collections import deque
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 n = int(input())
 table = [list(map(int,input().split())) for _ in range(n)]
 
-def dfs(x, y, h):
+def bfs(x,y,h):
     visited[x][y] = 1
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
-    for i in range(4):
-        mx, my = x+dx[i], y+dy[i]
-        if 0<=mx<n and 0<=my<n and visited[mx][my] == 0 and table[x][y] > h : dfs(mx,my,h)
+    queue = deque([(x,y)])
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            mx, my = x+dx[i], y+dy[i]
+            if 0<=mx<n and 0<=my<n and visited[mx][my] == 0 and table[x][y] > h:
+                visited[mx][my] = 1
+                queue.append((mx,my))
 
 answer = []
 for h in range(101):
@@ -21,7 +27,8 @@ for h in range(101):
         for y in range(n):
             if visited[x][y] == 0 and table[x][y] > h : 
                 cnt += 1
-                dfs(x,y,h)
+                # dfs(x,y,h)
+                bfs(x,y,h)
             else: visited[x][y] = 1
     answer.append(cnt)
     if cnt == 0 : break
