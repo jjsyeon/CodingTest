737fbad3f13480e5358cf3c309c602b2db2b0497
JSY <65614582+jjsyeon@users.noreply.github.com>
2025-02-19 11:14:07 +0900

    [Silver II] Title: 연속합, Time: 80 ms, Memory: 39096 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+dp = [arr[0]] + [0] * (n-1)
+
+for i in range(1,n):
+    dp[i] = max(dp[i-1]+arr[i], arr[i])
+
+print(max(dp))
