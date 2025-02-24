879fbd58b97f7e29eaea1a8de40435af0ecf015c
JSY <65614582+jjsyeon@users.noreply.github.com>
2025-02-24 15:30:22 +0900

    [Silver II] Title: 색종이 만들기, Time: 140 ms, Memory: 15544 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int [][] paper;
+    static int whitePaper, bluePaper;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        paper = new int [N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) paper[i][j] = Integer.parseInt(st.nextToken());
+        }
+        cntPaper(N, 0, 0);
+        System.out.println(whitePaper);
+        System.out.println(bluePaper);
+
+
+    }
+    static void cntPaper(int size, int x, int y) {
+        boolean isSame = true;
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (paper[i][j] != paper[x][y]) {
+                    isSame = false;
+                    break;
+                }
+            }
+        }
+        if (isSame) {
+            if (paper[x][y] == 1) bluePaper++;
+            else whitePaper++;
+            return;
+        }
+        cntPaper(size/2, x, y);
+        cntPaper(size/2, x + size/2, y);
+        cntPaper(size/2, x, y + size/2);
+        cntPaper(size/2, x + size/2, y + size/2);
+    }
+}
