391691a236e3a87937b06a6dfda420a1da7b64b4
JSY <65614582+jjsyeon@users.noreply.github.com>
2025-02-24 14:50:50 +0900

    [Silver III] Title: 칸토어 집합, Time: 240 ms, Memory: 31736 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            try {
+                int N = Integer.parseInt(br.readLine());
+                sb.append(dnc(N)).append("\n");
+            } catch (Exception e) {
+                break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    static String dnc(int N) {
+        if (N == 0) return "-";
+        StringBuilder sb = new StringBuilder();
+        return dnc(N - 1) + " ".repeat((int)(Math.pow(3, N-1))) + dnc(N - 1);
+    }
+}
