fe1b43b82b65ad4282bd4ca32c58fd69e25c9d11
JSY <65614582+jjsyeon@users.noreply.github.com>
2025-02-24 14:57:22 +0900

    [Silver III] Title: 칸토어 집합, Time: 184 ms, Memory: 25940 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -2,9 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static String[] history = new String[13];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        history[0] = "-";
         while (true) {
             try {
                 int N = Integer.parseInt(br.readLine());
@@ -17,9 +19,9 @@ public class Main {
     }
 
     static String dnc(int N) {
-        if (N == 0) return "-";
+        if (N == 0) return history[0];
         StringBuilder sb = new StringBuilder();
-        String tmp = dnc(N-1);
-        return sb.append(tmp).append(" ".repeat((int)(Math.pow(3, N-1)))).append(tmp).toString();
+        if (history[N-1] == null) history[N-1] = dnc(N-1);
+        return sb.append(history[N-1]).append(" ".repeat((int)(Math.pow(3, N-1)))).append(history[N-1]).toString();
     }
 }
