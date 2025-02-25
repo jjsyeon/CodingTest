5bcfc03b0fab4cd825494a0aa65fe00a798bb37e
JSY <65614582+jjsyeon@users.noreply.github.com>
2025-02-25 10:44:54 +0900

    [Silver I] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -5,9 +5,6 @@ A, B, C = map(int, input().split())
 
 def dnc(idx):
     if idx == 1 : return A % C
-    if idx % 2:
-        return (A * dnc(idx//2)**2) % C
-    else:
-        return (dnc(idx//2)**2)%C
+    return ((A if idx%2 else 1) * (dnc(idx//2)**2))% C
 
 print(dnc(B))
