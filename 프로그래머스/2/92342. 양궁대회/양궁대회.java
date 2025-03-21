91c96607b4c590cb5180fb7d78e2b0fc9233ef22
JSY <65614582+jjsyeon@users.noreply.github.com>
2025-03-21 09:44:50 +0900

    add comment 양궁대회.java
    문제 풀이에 대한 설명 추가

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,4 +1,3 @@
-
 public class Solution {
     int[] answer = {-1};
     int[] visited = new int[11];
@@ -13,25 +12,27 @@ public class Solution {
         return answer;
     }
 
-    public void backtrack(int cnt, int idx ){
-        if (cnt == arrow || idx < 0) {
-            int diff = calc();
-            if (max < diff || (diff == max && answer[answer.length-1] < arrow-cnt)) {
-                max = diff;
+    public static void backtrack(int cnt, int idx ){
+        if (cnt == arrow || idx < 0) { // 화살 다 쐈거나, 남은 화살 다 0점에 쏘는 경우
+            int diff = calc(); // 점수 차 계산
+            if (max < diff || (diff == max && answer[answer.length-1] < arrow-cnt)) {  // 지금 경우의 점수 차가 더 크거나
+                                                                                       // 점수 차는 동일하지만 지금 경우에 0을 쏜 개수가 더 많은 경우
+                max = diff; // 최대 차이 경우의수 업데이트
                 answer = visited.clone();
-                answer[10] = arrow-cnt;
+                answer[10] = arrow-cnt; // 0점은 남은 화살을 다 넣어야하므로 종료조건에 별도 처리
             }
         }
-
-        for (int i = idx; i >= 0; i--) {
+        // 점수가 낮은 것 부터 조회 (점수 차 같을 때 낮은 점수가 많은 경우를 반환해야하기 때문)
+        // 현재 점수 10-idx ~ 10점 까지 조회
+        for (int i = idx; i >= 0; i--) { 
             if (apeech[i] >= arrow - cnt) {continue;}
-            visited[i] = apeech[i] + 1;
-            backtrack(cnt + apeech[i] + 1, i-1);
-            visited[i] = 0;
+            visited[i] = apeech[i] + 1; // i(점수 10-i) 땄을 때 처리
+            backtrack(cnt + apeech[i] + 1, i-1); //  i(점수 10-i) 땄을 때의 가능한 경우의 수
+            visited[i] = 0; // i(점수 10-i) 안 딴 상태로 변경
         }
     }
 
-    public int calc() {
+    public static int calc() {
         int score = 0;
         for (int i = 0; i < 11; i++) {
             if (visited[i] == 0 && apeech[i] == 0) continue;
