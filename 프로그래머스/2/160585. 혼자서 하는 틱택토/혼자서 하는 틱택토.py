8143fc48a198396d19e6995cb3897ef51259ce7c
JSY <65614582+jjsyeon@users.noreply.github.com>
2025-05-13 16:44:17 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
@@ -2,54 +2,19 @@ def solution(board):
     board_str = "".join(board)
 
     def win_case(mark):
-        point = {"row": [0,0,0], "col": [0,0,0], "dig": [0,0]}
-        for idx, case in enumerate(board_str):
-            r,c = idx // 3, idx % 3
-            if case == mark:
-                point["row"][r] += 1
-                point["col"][c] += 1
-                if r==1 and c == 1:
-                    point["dig"][0] += 1
-                    point["dig"][1] += 1  
-                elif r == c: point["dig"][0] += 1
-                elif r+c == 2 and r*c ==0: point["dig"][1] += 1  
-
-        point["row"] = [p//3 for p in point["row"]]
-        point["col"] = [p//3 for p in point["col"]]
-        point["dig"] = [p//3 for p in point["dig"]]
+        point = [0,0,0]
+        # 가로, 세로 점수 조사
+        for i in range(3):
+            if board[i] == mark * 3 : point[0] += 1 # 가로 조사
+            if "".join([item[i] for item in board]) == mark * 3 : point[1] += 1 # 세로 조사
+        # 대각선 점수 조사
+        if "".join(board[i][i] for i in range(3)) == mark * 3 : point[2] += 1
+        if "".join(board[i][2-i] for i in range(3)) == mark * 3 : point[2] += 1
         
-        # 점수 없음
-        if (sum(point["row"]) + sum(point["col"]) + sum(point["dig"])) == 0: return 0
-    
-        # 가로/세로/대각선 점수 중 하나만 나옴
-        elif (sum(point["row"]) + sum(point["col"]) + sum(point["dig"])) == 1: return 1
-            
-        # 가로/세로 점수 두개 이상
-        elif sum(point["row"]) > 1 or sum(point["col"]) > 1: return -1
-        
-        # 대각선 점수 둘일 때
-        # if point["dig"] == [1,1]: 
-            # if(sum(point["row"]) + sum(point["col"])) == 0 : return 1
-            # if point["row"] not in [[0,0,0], [0,1,0]] or point["col"] not in [[0,0,0], [0,1,0]] :return -1
-            # else : return 1
-        
-        # 가로/세로/대각선 점수 하나씩 나온 케이스
-#         elif point["dig"] == [1,0]:
-#             if [point["row"], point["col"]] in [[[1,0,0],[0,0,0]],
-#                                                 [[0,0,1],[0,0,0]],
-#                                                 [[0,0,0],[0,0,1]],
-#                                                 [[0,0,0],[1,0,0]]]: return 1
-#             else: return -1
-            
-#         elif point["dig"] == [0,1]:
-#             if [point["row"], point["col"]] in [[[1,0,0],[0,0,0]],
-#                                                 [[0,0,1],[0,0,0]],
-#                                                 [[0,0,0],[0,0,1]],
-#                                                 [[0,0,0],[1,0,0]]]: return 1
-#             else: return -1
-        
-        # 가로/세로 점수 하나씩 
+        if sum(point) == 0: return 0
+        # elif point[0] > 1 or point[1] > 1: return -1
         return 1
+        
     
     # 게임 시작 안됨
     if "."*9 == board_str : return 1
